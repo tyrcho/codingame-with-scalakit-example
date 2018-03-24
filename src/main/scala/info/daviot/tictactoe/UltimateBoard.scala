2f7f5e28b89296c57f9d491dc15592b101752518
Michel Daviot <michel@daviot.info>
2018-03-24 18:41:09 +0100

    :sparkles: UltimateBoard proper moves and debug text

--- src/main/scala/info/daviot/tictactoe/UltimateBoard.scala
+++ src/main/scala/info/daviot/tictactoe/UltimateBoard.scala
@@ -3,42 +3,70 @@ package info.daviot.tictactoe
 import com.truelaurel.math.geometry.Pos
 import com.truelaurel.samplegames.gomoku.GomokuBoard
 
-case class UltimateBoard(smallBoards: Map[Pos, GomokuBoard] = Map.empty, lastMove: Option[Pos] = None) {
-
-  def play(pos: Pos): UltimateBoard = {
-    val key = pos / 3
-    val smallBoard = boardToPlay(key).play(pos % 3)
-    copy(
-      lastMove = Some(pos),
-      smallBoards = smallBoards.updated(key, smallBoard))
-  }
-
-  def validMoves: Set[Pos] =
-    (lastMove match {
-      case None => for {
-        row <- 0 to 8
-        col <- 0 to 8
-      } yield Pos(row, col)
-      case Some(last) =>
-        val smallBoard = nextSmallBoard.get
-        for {
-          row <- last.x * 3 until (1 + last.x) * 3
-          col <- last.y * 3 until (1 + last.y) * 3
-          pos = Pos(row, col)
-          if smallBoard.isFree(pos % 3)
-        } yield pos
-    }).toSet
+case class UltimateBoard(smallBoards: Map[Pos, GomokuBoard] = Map.empty,
+                         lastMove: Option[Pos] = None,
+                         lastPlayer: Boolean = true) {
 
 
-
-  private def boardToPlay(p: Pos): GomokuBoard =
-    smallBoards.getOrElse(p / 3, emptySmallBoard)
-
-  private def nextSmallBoard: Option[GomokuBoard] =
-    lastMove.map { lastPos =>
-      smallBoards.getOrElse(lastPos % 3, emptySmallBoard)
+    def play(pos: Pos): UltimateBoard = {
+        val key = pos / 3
+        val oldSmallBoard = boardToPlay(pos)
+        val smallBoard = oldSmallBoard.forcePlay(pos % 3, !lastPlayer)
+        copy(
+            lastMove = Some(pos),
+            smallBoards = smallBoards.updated(key, smallBoard),
+            lastPlayer = !lastPlayer)
     }
 
-  val emptySmallBoard = GomokuBoard(3)
+    def validMoves: Set[Pos] =
+        (lastMove match {
+            case None => for {
+                row <- 0 to 8
+                col <- 0 to 8
+            } yield Pos(row, col)
+            case Some(last) =>
+                val smallBoard = nextSmallBoard.get
+                for {
+                    row <- last.x * 3 until (1 + last.x) * 3
+                    col <- last.y * 3 until (1 + last.y) * 3
+                    pos = Pos(row, col)
+                    if smallBoard.isFree(pos % 3)
+                } yield pos
+        }).toSet
 
+
+    private def boardToPlay(p: Pos): GomokuBoard =
+        smallBoards.getOrElse(p / 3, emptySmallBoard)
+
+    private def nextSmallBoard: Option[GomokuBoard] =
+        lastMove.map { lastPos =>
+            smallBoards.getOrElse(lastPos % 3, emptySmallBoard)
+        }
+
+    val emptySmallBoard = GomokuBoard(3)
+
+    val separatorLine: String = "-" * 11
+
+    def debugString: String =
+        (((0 to 2).map(debugRow) :+ separatorLine) ++
+            ((3 to 5).map(debugRow) :+ separatorLine) ++
+            (6 to 8).map(debugRow)).mkString("\n")
+
+
+    def debugRow(row: Int): String =
+        (((0 to 2).map(debugPos(row)) :+ '|') ++
+            ((3 to 5).map(debugPos(row)) :+ '|') ++
+            (6 to 8).map(debugPos(row))).mkString
+
+    def debugPos(row: Int)(col: Int): Char = {
+        val pos = Pos(row, col)
+        val board = smallBoards.getOrElse(pos / 3, emptySmallBoard)
+        val smallPos = pos % 3
+        if (board.isFree(smallPos)) '.'
+        else if (board.playedFalse(smallPos)) firstPlayerChar
+        else secondPlayerChar
+    }
+
+    val firstPlayerChar = 'X'
+    val secondPlayerChar = 'O'
 }
--- src/test/scala/info/daviot/tictactoe/UltimateBoardTest.scala
+++ src/test/scala/info/daviot/tictactoe/UltimateBoardTest.scala
@@ -1,32 +1,77 @@
 package info.daviot.tictactoe
 
+import com.github.writethemfirst.approvals.Approver
 import com.truelaurel.math.geometry.Pos
+import com.truelaurel.samplegames.gomoku.GomokuBoard
 import org.scalatest.{FlatSpec, Matchers}
 
 class UltimateBoardTest extends FlatSpec with Matchers {
-  val emptyBoard = UltimateBoard()
+    val emptyBoard = UltimateBoard()
 
-  "board" should "produce 81 initial valid moves" in {
-    val moves = emptyBoard.validMoves
-    moves should have size 81
-  }
+    "small board" should "allow 2 moves for same player" in {
+        val played = GomokuBoard(3)
+            .forcePlay(Pos(0, 1), true)
+            .forcePlay(Pos(0, 2), true)
+        new Approver().writeTo("smallBoard").verify(played.toText)
+    }
 
-  it should "produce 9 valid moves after first play" in {
-    val moves = emptyBoard.play(Pos(1, 0)).validMoves
-    moves shouldBe Set(
-      Pos(3, 0), Pos(3, 1), Pos(3, 2),
-      Pos(4, 0), Pos(4, 1), Pos(4, 2),
-      Pos(5, 0), Pos(5, 1), Pos(5, 2)
-    )
-  }
+    "board" should "produce 81 initial valid moves" in {
+        val moves = emptyBoard.validMoves
+        moves should have size 81
+    }
 
-  it should "produce 8 valid moves after first play at 0,0" in {
-    val moves = emptyBoard.play(Pos(0, 0)).validMoves
-    moves shouldBe Set(
-      Pos(0, 1), Pos(0, 2),
-      Pos(1, 0), Pos(1, 1), Pos(1, 2),
-      Pos(2, 0), Pos(2, 1), Pos(2, 2)
-    )
-  }
+    it should "produce 9 valid moves after first play" in {
+        val moves = emptyBoard.play(Pos(1, 0)).validMoves
+        moves shouldBe Set(
+            Pos(3, 0), Pos(3, 1), Pos(3, 2),
+            Pos(4, 0), Pos(4, 1), Pos(4, 2),
+            Pos(5, 0), Pos(5, 1), Pos(5, 2)
+        )
+    }
+
+    it should "produce 8 valid moves after first play at 0,0" in {
+        val moves = emptyBoard.play(Pos(0, 0)).validMoves
+        moves shouldBe Set(
+            Pos(0, 1), Pos(0, 2),
+            Pos(1, 0), Pos(1, 1), Pos(1, 2),
+            Pos(2, 0), Pos(2, 1), Pos(2, 2)
+        )
+    }
+
+
+    it should "debug properly" in {
+        val moves = Seq(Pos(3, 2), Pos(1, 7), Pos(5, 4), Pos(6, 4), Pos(2, 4),
+            Pos(7, 4), Pos(4, 4), Pos(5, 3), Pos(6, 0), Pos(2, 2), Pos(7, 6),
+            Pos(5, 2), Pos(6, 6), Pos(0, 2), Pos(0, 6), Pos(1, 2))
+
+        moves.zipWithIndex.foldLeft(emptyBoard) {
+            case (board, (pos, i)) =>
+                val next = board.play(pos)
+                new Approver().writeTo("properDebug" + (i + 1)).verify(next.debugString)
+                next
+        }
+    }
+
+    //  it should "allow to play anywhere when sent on a completed small board" in {
+    //    val board = emptyBoard
+    //      .play(Pos(3, 2)) // ..O| ...|X..
+    //      .play(Pos(1, 7)) // ..O| ...|.O.
+    //      .play(Pos(5, 4)) // ..O| .X.|...
+    //      .play(Pos(6, 4)) // ------------
+    //      .play(Pos(2, 4)) // ..X| ...|...
+    //      .play(Pos(7, 4)) // ...| .X.|...
+    //      .play(Pos(4, 4)) // ..O| OX.|...
+    //      .play(Pos(5, 3)) // ------------
+    //      .play(Pos(6, 0)) // X..| .O.|X..
+    //      .play(Pos(2, 2)) // ...| .O.|X..
+    //      .play(Pos(7, 6)) // ...| ...|...
+    //      .play(Pos(5, 2))
+    //      .play(Pos(5, 2))
+    //      .play(Pos(6, 6))
+    //      .play(Pos(0, 2))
+    //      .play(Pos(1, 2))
+    //
+    //
+    //  }
 
 }
