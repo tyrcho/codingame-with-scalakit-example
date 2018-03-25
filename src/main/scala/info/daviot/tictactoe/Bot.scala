7f6b72310d448e0b35608895b423973fe9a09c6b
Michel Daviot <michel@daviot.info>
2018-03-25 18:47:47 +0200

    :zap: can take up to 1 second for first move

--- src/main/scala/info/daviot/tictactoe/Bot.scala
+++ src/main/scala/info/daviot/tictactoe/Bot.scala
@@ -1,17 +1,13 @@
 package info.daviot.tictactoe
 
-import com.truelaurel.algorithm.alphabeta.AlphaBetaAi
 import com.truelaurel.algorithm.mcts.MctsAi
 import com.truelaurel.codingame.challenge.GameBot
 import com.truelaurel.math.geometry.Pos
-import com.truelaurel.samplegames.gomoku.{GomokuBoard, GomokuRules}
 import com.truelaurel.time.Chronometer
 
 import scala.concurrent.duration.DurationInt
 
 object Bot extends GameBot[State, Action] {
-    val rules = GomokuRules(3, 3)
-
     /**
       * Reacts to the given game state by playing one or more actions
       *
@@ -22,11 +18,9 @@ object Bot extends GameBot[State, Action] {
         Action(mctsMove(state.board))
 
     def mctsMove(s: UltimateBoard): Pos = {
-        val chronometer = new Chronometer(90.millis)
+        val time = if (s.lastMove.isDefined) 90.millis else 900.millis
+        val chronometer = new Chronometer(time)
         chronometer.start()
         MctsAi(UltimateRules)(_ => chronometer.willOutOfTime).chooseMove(s)
     }
-
-    def alphaBetaMove(s: GomokuBoard): Pos =
-        AlphaBetaAi(rules, rules.centerHeuristic).chooseMove(s, 3)
 }
